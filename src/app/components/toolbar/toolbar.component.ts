3d3904a00772e28128674074d4c10dbf7ecc10a6
Vinicius Branco <viniblima2016@gmail.com>
2024-02-09 15:11:16 -0300

    feat: :sparkles: adiciona estilizacao da toolbar
    adicionando cor à toolbar e clique para abrir sidenav

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,10 +3,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-
   @Output() onClickButtonSideNav = new EventEmitter<void>();
 
+  clickMenu(): void {
+    this.onClickButtonSideNav.emit();
+  }
 }
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  url: 'http://localhost:3000/api/v1',
+  // url: 'http://localhost:3000/api/v1',
+  url: 'https://nu-seal-backend-5befaec36a8f.herokuapp.com/api/v1',
 };
