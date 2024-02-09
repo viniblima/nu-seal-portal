b37777da4870b38e98f51d5d5473056d3b2a40cd
Vinicius Branco <viniblima2016@gmail.com>
2024-02-09 16:04:42 -0300

    fix: :bug: altera pasta de build do angular

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/nu-seal-portal/browser/'));
 
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/' });
-  });
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/nu-seal-portal/browser/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
