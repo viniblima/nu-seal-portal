379bb8d37aee241520d642632cb9c8e49e9875bd
Vinicius Branco <viniblima2016@gmail.com>
2024-02-09 15:58:49 -0300

    feat: :bug: altera pasta de build do projeto

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/nu-seal-portal'));
+app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/nu-seal-portal/'}),
-);
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/' });
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
